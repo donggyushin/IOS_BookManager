ec87ba76e97ab553b143283701b26bf224b817c0
좌 준협 <jwajunhyeob@jwaui-MacBook-Air.local>
2018-10-05 21:56:48 +0900

    render book list

--- MyBooks/ViewController.swift
+++ MyBooks/ViewController.swift
@@ -1,18 +1,33 @@
-//
-//  ViewController.swift
-//  MyBooks
-//
-//  Created by 좌 준협 on 2018. 10. 5..
-//  Copyright © 2018년 좌 준협. All rights reserved.
-//
+
 
 import UIKit
 
 class ViewController: UIViewController {
+    var MyBookManager = BookManager()
+    @IBOutlet weak var textView : UITextView?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        var book1 = Book(name:"The Lord of The Rings", genre: "Fantasy", author:"J.R.R Tolkien")
+        var book2 = Book(name:"Harry Potter", genre: "Fantasy", author: "J.K Rowling")
+        var book3 = Book(name:"Justice : What's the right thing to do", genre: "philosophy", author:"Michael J. Sandel")
+        
+        //book1.printBook()
+        //book2.printBook()
+        //book3.printBook()
+        
+        
+        
+        MyBookManager.registerBook(bookObject:book1)
+        MyBookManager.registerBook(bookObject:book2)
+        MyBookManager.registerBook(bookObject:book3)
+    }
+    
+    @IBAction func showAllBooks(_ sender:Any){
+        print("showAllBooks Button clicked!")
+        var bookList = MyBookManager.showBooks()
+        textView?.text = bookList
     }
 
     override func didReceiveMemoryWarning() {
